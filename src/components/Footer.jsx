f40c17de01d7b0b421a605ffef57c787ab5d364a
LAPTOP-OJ58J4QE\Nitro <12timezafnzthx@gmail.com>
2026-02-08 18:10:30 +0700

    fix bug of footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <section id="Footer">
+        <div className="footer-container">
+            <p className='contact'>CONTACT</p>
+            <p>Email : <a href="https://mail.google.com/mail/?view=cm&fs=1&to=12timezafnzthx@gmail.com" target="_blank"> 12timezafnzthx@gmail.com</a></p>
+            <p>Github : <a href="https://github.com/1naBUrBabe"> 1naBUrBabe</a></p>
+            <p>Phone : 064-969-3458</p>
+            <p>Address : Sadao District, Songkhla 90170, Thailand</p>
+            <p>Facebook : <a href='https://www.facebook.com/1naBUrBabe'>FI LM</a></p>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
